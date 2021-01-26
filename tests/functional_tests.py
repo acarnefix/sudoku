31c9369f1798698d4ce87c6e90390f12c97d659d
Aaron Carnefix <25955012+acarnefix@users.noreply.github.com>
2021-01-26 00:27:50 -0600

    Finish chapter 4

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
+import time
+# import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,6 +15,24 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_game(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("Sudoku", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("Sudoku", header_text)
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1: Buy peacock feathers" for row in rows),
+            "New to-do item did not appear in table."
+            )
+
+
         self.fail("Finish the test!")
 
 
