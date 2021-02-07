d4f939a5a1a41396a5d903e46776ade20392a962
Aaron Carnefix <25955012+acarnefix@users.noreply.github.com>
2021-02-07 17:09:41 -0600

    Use use form in home_page, simplify tests

--- sudoku/tests/test_views.py
+++ sudoku/tests/test_views.py
@@ -1,14 +1,20 @@
 from django.test import TestCase
 from django.utils.html import escape
 
+from sudoku.forms import ItemForm
 from sudoku.models import Item, List
 from sudoku.views import home_page
+
 
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get("/")
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class ListViewTest(TestCase):
